754389acad02ef387068eca285e164c16ed8d96f
Uther <uther@terra.money>
2020-02-15 22:29:25 +0900

    AI-193.5233.102.40.6137316 <uther@utherui-MacBookPro.local Create baseRefactoring.xml, vim_settings.xml, ide.general.xml, project.default.xml, Singleton.java, Enum.java, Interface.java, Default.xml, visualizationTool.xml, androidStudioFirstRun.xml, find.xml, filetypes.xml, colors.scheme.xml, kotlinRefactoring.xml, notifications.xml, path.macros.xml, github.xml, AnnotationType.java, studioFlags.xml, Class.java, jdk.table.xml, bloc༏ext༏..kt, laf.xml, Mac OS X 10_5_ copy.xml, vcs.xml, keymap.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/bloc༏ext༏..kt
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
+
+#end
+#parse("File Header.java")
+class ${NAME}Bloc:Bloc<${NAME}Event, ${Name}State>({${NAME}State()}) {
+}
+class ${NAME}Event {
+}
+class ${NAME}State {
+}
